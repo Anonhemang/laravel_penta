e7c58dbf56431118796679189e471084eb74d3db
hemang <hemangthakkar2801@gmail.com>
2024-01-23 11:06:23 +0530

    Updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 
 /*
@@ -33,6 +34,10 @@ Route::group(['prefix' => 'admin'], function () {
 
         Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
         Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+
+        // Categories Route
+        Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 
     });
 });
